709de477a25f98ffb9bfa5f5807c8e5d522b94d5
Elijah Miller <elijah.miller@gmail.com>
2016-10-18 12:34:40 -0400

    Remove name field from examples.

--- examples/sync.rb
+++ examples/sync.rb
@@ -28,7 +28,6 @@ begin
     test:             true,                                         # test documents are free but watermarked
     document_content: "<html><body>Hello World</body></html>",      # supply content directly
     # document_url:   "http://docraptor.com/examples/invoice.html", # or use a url
-    name:             "docraptor-ruby.pdf",                         # help you find a document later
     document_type:    "pdf",                                        # pdf or xls or xlsx
     # javascript:       true,                                       # enable JavaScript processing
     # prince_options: {
