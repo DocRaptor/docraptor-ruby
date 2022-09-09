8dc0c72b2980349a842cfded9437b0d437a863a6
James Paden <james@jamespaden.com>
2022-09-09 18:17:45 -0400

    Update with latest code generator files

--- examples/async.rb
+++ examples/async.rb
@@ -5,47 +5,42 @@
 # 10 minutes. This is useful when creating many documents in parallel, or very
 # large documents with lots of assets.
 #
-# DocRaptor supports many options for output customization, the full list is
-# https://docraptor.com/documentation/api#api_general
+# DocRaptor supports many CSS and API options for output customization. Visit
+# https://docraptor.com/documentation/ for full details.
 #
 # You can run this example with: ruby async.rb
 
-require "bundler/setup"
-Bundler.require
+require "docraptor"
 
-DocRaptor.configure do |dr|
-  dr.username  = "YOUR_API_KEY_HERE" # this key works for test documents
-  # dr.debugging = true
+DocRaptor.configure do |config|
+  config.username = "YOUR_API_KEY_HERE" # this key works in test mode!
 end
 
-$docraptor = DocRaptor::DocApi.new
+docraptor = DocRaptor::DocApi.new
 
 begin
-
-  # https://docraptor.com/documentation/api#api_general
-  create_response = $docraptor.create_async_doc(
-    test:             true,                                         # test documents are free but watermarked
-    document_content: "<html><body>Hello World</body></html>",      # supply content directly
-    # document_url:   "http://docraptor.com/examples/invoice.html", # or use a url
-    name:             "docraptor-ruby.pdf",                         # help you find a document later
-    document_type:    "pdf",                                        # pdf or xls or xlsx
-    # javascript:       true,                                       # enable JavaScript processing
+  # different method than the synchronous documents
+  response = docraptor.create_async_doc(
+    test: true, # test documents are free but watermarked
+    document_type: "pdf",
+    document_content: "<html><body>Hello World!</body></html>",
+    # document_url: "https://docraptor.com/examples/invoice.html",
+    # javascript: true,
     # prince_options: {
-    #   media: "screen",                                            # use screen styles instead of print styles
-    #   baseurl: "http://hello.com",                                # pretend URL when using document_content
-    # },
+    #   media: "print", # @media 'screen' or 'print' CSS
+    #   baseurl: "https://yoursite.com", # the base URL for any relative URLs
+    # }
   )
 
   loop do
-    status_response = $docraptor.get_async_doc_status(create_response.status_id)
-    puts "doc status: #{status_response.status}"
+    status_response = docraptor.get_async_doc_status(response.status_id)
+    puts "status: #{status_response.status}"
     case status_response.status
     when "completed"
-      doc_response = $docraptor.get_async_doc(status_response.download_id)
-      File.open("/tmp/docraptor-ruby.pdf", "wb") do |file|
-        file.write(doc_response)
-      end
-      puts "Wrote PDF to /tmp/docraptor-ruby.pdf"
+      document = docraptor.get_async_doc(status_response.download_id)
+      # get_async_doc() returns a binary string
+      File.write("github-async.pdf", document, mode: "wb")
+      puts "Successfully created github-async.pdf!"
       break
     when "failed"
       puts "FAILED"
@@ -55,10 +50,9 @@ begin
       sleep 1
     end
   end
-
 rescue DocRaptor::ApiError => error
   puts "#{error.class}: #{error.message}"
-  puts error.code          # HTTP response code
-  puts error.response_body # HTTP response body
+  puts error.code
+  puts error.response_body
   puts error.backtrace[0..3].join("\n")
 end
